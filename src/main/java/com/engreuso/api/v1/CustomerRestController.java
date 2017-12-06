8cdbb60698deb96463cc7ab0327d921b6606bb00
anderson.marques <anderson.marques@CPX-Z0350FRJLYQ.dir.svc.accenture.com>
2017-12-06 17:13:03 -0300

    Criação dos endpoints de Customer

--- src/main/java/com/engreuso/api/v1/CustomerRestController.java
+++ src/main/java/com/engreuso/api/v1/CustomerRestController.java
@@ -1,8 +1,5 @@
 package com.engreuso.api.v1;
 
-import java.util.ArrayList;
-import java.util.List;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.PathVariable;
